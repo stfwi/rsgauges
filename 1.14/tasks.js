b59a334d3893d8103e1d4162778e82b618903ca9
stfwi <cerberos@atwillys.de>
2019-08-02 22:01:10 +0200

    Build system: Version history generation improved, temporary 1.14 build allowed from the dev repository.

--- 1.14/tasks.js
+++ 1.14/tasks.js
@@ -2,6 +2,7 @@
 // Note for reviewers/clones: This file is a auxiliary script for my setup. It's not needed to build the mod.
 "use strict";
 const constants  = include("../meta/lib/constants.js")();
+constants.options.without_ref_repository_check = true;
 const libtask    = include("../meta/lib/libtask.js")(constants);
 const liblang    = include("../meta/lib/liblang.1.13.js")(constants); // 1.14 lang same as 1.13
 const libassets  = include("../meta/lib/libassets.js")(constants);
